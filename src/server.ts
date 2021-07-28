3482b4104e2fb8b786af1cbc3c820ab4c91fd015
Israel Alagbe <israelalagbe53@gmail.com>
2021-07-28 23:50:43 +0100

    server cleanup

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import app from "./app";
 import env from "./config/env";
 
-// try {
-//   const port = env.port;
-//   app.listen(port, (): void => {
-//     console.log(`Connected successfully on port ${port}`);
-//   });
-// } catch (error) {
-//   console.error(`Error occured: ${error.message}`);
-// }
+try {
+  const port = env.port;
+  app.listen(port, (): void => {
+    console.log(`Connected successfully on port ${port}`);
+  });
+} catch (error) {
+  console.error(`Error occured: ${error.message}`);
+}
--- src/util/sequelize.ts
+++ src/util/sequelize.ts
@@ -2,7 +2,6 @@ import { Dialect, Sequelize } from 'sequelize';
 import env from '../config/env';
 
 const database = require('../config/database')[env.name];
-
 const sequelize = new Sequelize(
   database.database, database.username, database.password, {
   dialect: database.dialect as Dialect,
