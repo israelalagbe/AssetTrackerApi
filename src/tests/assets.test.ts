52141f51b2b39b191bbb16d83dc7a816b45cf2f0
Israel Alagbe <israelalagbe53@gmail.com>
2021-07-28 21:20:55 +0100

    Added test for assets

--- src/tests/assets.test.ts
+++ src/tests/assets.test.ts
@@ -10,9 +10,7 @@ let server: http.Server;
 beforeAll((done) => {
   const port = env.port;
   server = app.listen(port, (): void => {
-    sequelize.sync({ force: true }).then(() => {
-      done();
-    });
+    done();
   });
 });
 
@@ -21,24 +19,66 @@ afterAll((done) => {
     done();
   });
 });
-
+ 
+let assets: Asset[];
 beforeEach(async () => {
-    jest.m
-  await Asset.bulkCreate([{ name: "Car", latitude: "2.39494", longitude: "2.39494" }]);
+  await sequelize.sync({ force: true });
+  assets = await Asset.bulkCreate([{ name: "Car", latitude: "2.39494", longitude: "2.39494" }]);
 });
 
 describe("Asset Test", () => {
-  it("[GET] /assets", (done) => {
+  it("should get all assets", (done) => {
     request(app)
       .get("/assets")
       .end(function (err, res) {
         expect(res.statusCode).toBe(200);
-        expect(res.body).toMatchObject([{ name: "Car", latitude: "2.39494", longitude: "2.39494" }])
+        expect(res.body).toMatchObject([
+          { name: "Car", latitude: "2.39494", longitude: "2.39494" },
+        ]);
         done();
       });
   });
-});
 
-afterAll(async () => {
-  // await new Promise(resolve => setTimeout(() => resolve(), 100));
+  it("should create an asset", (done) => {
+    request(app)
+      .post("/assets")
+      .send({
+        name: "Bus",
+        latitude: "2.4494",
+        longitude: "2.39594",
+      })
+      .end(async (err, res) => {
+        expect(res.statusCode).toBe(201);
+        expect(res.body).toMatchObject({
+          data: { name: "Bus", latitude: "2.4494", longitude: "2.39594" },
+        });
+        
+        const assets = await Asset.findAll({});
+        expect(assets.length).toBe(2);
+
+        done();
+      });
+  });
+
+  it("should update an asset", (done) => {
+    const asset = assets[0];
+    const location = {
+        latitude: "5.4494",
+        longitude: "6.39594",
+    };
+
+    request(app)
+      .put(`/assets/${asset.id}/location`)
+      .send(location)
+      .end(async (err, res) => {
+        expect(res.statusCode).toBe(201);
+        expect(res.body).toMatchObject({
+          data: location,
+        });
+        
+        const asset = await Asset.findAndCountAll({where: location});
+        expect(asset.count).toBe(1);
+        done();
+      });
+  });
 });
